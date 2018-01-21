b251710e1553c0a97587c4dfc68babb029ba7783
Austin <robbi138@msu.edu>
2018-01-20 19:14:04 -0500

    added maps activity

--- app/src/main/java/com/example/austin/trashapp/MapsActivity.java
+++ app/src/main/java/com/example/austin/trashapp/MapsActivity.java
@@ -1,10 +1,18 @@
 package com.example.austin.trashapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -12,10 +20,28 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private LocationManager locationManager = null;
+    private SharedPreferences settings = null;
+
+    private double latitude = 0;
+    private double longitude = 0;
+    private float prevDistance = 0;
+    private boolean valid = false;
+
+    private double toLatitude = 0;
+    private double toLongitude = 0;
+    private int points = 0;
+
+    private final static String TOLAT = "tolat";
+    private final static String TOLONG = "tolong";
+    private final static String POINTS = "points";
+
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Get the location manager
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -38,9 +64,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Create a Criteria object
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+        criteria.setAltitudeRequired(true);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(false);
+
+        Location location;
+        String bestAvailable = locationManager.getBestProvider(criteria, true);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        location = locationManager.getLastKnownLocation(bestAvailable);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        final SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        LatLng selfLoc = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(selfLoc).title("You"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(selfLoc));
+        mapFragment.onResume();
     }
+
 }
