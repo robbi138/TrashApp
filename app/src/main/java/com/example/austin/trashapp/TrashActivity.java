744c685c17e5d0fcfbaa841026effab426e278d7
Austin <robbi138@msu.edu>
2018-01-20 19:25:37 -0500

    big memes

--- app/src/main/java/com/example/austin/trashapp/TrashActivity.java
+++ app/src/main/java/com/example/austin/trashapp/TrashActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class TrashActivity extends AppCompatActivity {
 
+    private static final String CREATE_CAN = "http://webdev.cse.msu.edu/~robbi138/classweb/trashApp/insertCan.php";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
